aaa38cf14d594e9477c0aae6f9c54a476ed4579f
Dashamir Hoxha <dashohoxha@gmail.com>
2016-07-03 16:31:44 +0200

    Import data and a few translations.

--- modules/custom/qtrCore/data/install.sh
+++ modules/custom/qtrCore/data/install.sh
@@ -6,3 +6,11 @@ cd $(dirname $0)
 ### create the DB tables
 mysql=$(drush sql-connect)
 $mysql < db/qtr_schema.sql
+
+### import chapters, verses, and a few english translations
+data='/var/www/data'
+drush eval "qtr::import_chapters('$data/quran/index.xml');"
+drush eval "qtr::import_verses('$data/quran/uthmani.xml');"
+drush eval "qtr::import_translations('en', '$data/translations/en/pickthall.txt');"
+drush eval "qtr::import_translations('en', '$data/translations/en/shakir.txt');"
+drush eval "qtr::import_translations('en', '$data/translations/en/yusufali.txt');"
