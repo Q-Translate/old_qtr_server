bda76749b8009946d3622ce158b2c0b311b403cb
Dashamir Hoxha <dashohoxha@gmail.com>
2016-07-24 01:35:58 +0200

    Improve the import script to run with nohup.

--- modules/custom/qtrCore/data/import.sh
+++ modules/custom/qtrCore/data/import.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-### get the language code
-if [[ -z $1 ]]; then
-    echo "
-Usage: $0 <lng>
+### go to the directory of the script
+cd $(dirname $0)
 
-where <lng> is the language code, like: en, fr, it, de, sq, etc.
-"
-    exit 1
+### make sure that the script is called with `nohup nice ...`
+if [[ "$1" != "--dont-fork" ]]; then
+    # this script has *not* been called recursively by itself
+
+    ### get the language code
+    if [[ -z $1 ]]; then
+        echo "Usage: $0 <lng>
+        where <lng> is the language code, like: en, fr, it, de, sq, etc."
+        exit 1
+    fi
+    lng=$1
+
+    datestamp=$(date +%F | tr -d -)
+    nohup_out=logs/nohup-import-$lng-$datestamp.out
+    mkdir -p logs/
+    rm -f $nohup_out
+    nohup nice $0 --dont-fork $@ > $nohup_out &
+    sleep 1
+    echo "tail -f $(pwd)/$nohup_out"
+    tail -f $nohup_out
+    exit
+else
+    # this script has been called by itself
+    shift # remove the flag $1 that is used as a termination condition
 fi
+
+### get the language code
 lng=$1
 
 ### go to the directory of translations
-cd $(dirname $0)
 mkdir -p translations/
 cd translations/
 
@@ -23,8 +43,9 @@ for file in $files; do
     echo "Get $file"
     rm -f $file
     wget -q $translations/$file
+    sed -i $file -e '/#/d' -e '/^$/d'
 
     echo "Import $file"
     author=${file%.txt}
-    drush qtr-import $lng $author "$(pwd)/$file" --force
+    time drush qtr-import $lng $author "$(pwd)/$file" --force
 done
