169b06ef6a315f0397a97e3dbb57c64848591705
Dashamir Hoxha <dashohoxha@gmail.com>
2016-07-31 12:04:40 +0200

    Fix tweet

--- utils/twitter.sh
+++ utils/twitter.sh
@@ -4,7 +4,7 @@
 ### http://xmodulo.com/2013/12/access-twitter-command-line-linux.html
 
 t='/usr/local/bin/t'
-lng=sq
+lng=en
 tweet=$(curl -k https://qtranslate.org/tweet/$lng)
 mention=$( ( $t followings ; $t followers ) | uniq | sort -R | tail -1)
 $t update "$tweet @$mention"
