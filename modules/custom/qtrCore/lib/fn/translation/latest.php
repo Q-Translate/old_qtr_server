5124388ff1ffe8d815e40ab5fc5087062601a4cb
Dashamir Hoxha <dashohoxha@gmail.com>
2016-08-02 02:43:26 +0200

    Make the function of latest translations more flexible.

--- modules/custom/qtrCore/lib/fn/translation/latest.php
+++ modules/custom/qtrCore/lib/fn/translation/latest.php
@@ -8,12 +8,21 @@ namespace QTranslate;
 use \qtr;
 
 /**
- * Get and return an array of the latest translation suggestions,
- * submitted between the last midnight and the midnight before.
- * The fields that are returned are:
- *   tname, cid, vid, string, lng, translation, tguid, time, name, umail
+ * Get and return an array of the latest translation suggestions.
+ *
+ * If the option $days is given, return all the translations since that many
+ * days ago. Otherwise, only the translations since yesterday.
+ *
+ * If the option $limit is given, then return that many translations from the
+ * latest ones, regardless of their time. This takes precedence over the option
+ * $days.
+ *
+ * Each of the returned translations has these fields:
+ *   chapter_id, verse_nr, vid, lng, translation, tguid, time, username, usermail
  */
-function translation_latest($lng) {
+function translation_latest($lng, $days = NULL, $limit = NULL) {
+
+  if ($days == NULL) $days = 1;
 
   $get_latest_translations = "
     SELECT v.cid AS chapter_id, v.nr AS verse_nr, v.vid,
@@ -23,16 +32,20 @@ function translation_latest($lng) {
     LEFT JOIN {qtr_verses} v ON (v.vid = t.vid)
     LEFT JOIN {qtr_users} u ON (u.umail = t.umail AND u.ulng = t.ulng)
     LEFT JOIN {qtr_chapters} c ON (c.cid = v.cid)
-    WHERE t.umail != '' AND t.lng = :lng AND t.time > :from_date
-    ORDER BY t.time DESC
-    ";
+    WHERE t.umail != '' AND t.lng = :lng";
+  $args[':lng'] = $lng;
 
-  $args = array(
-    ':lng' => $lng,
-    ':from_date' => date('Y-m-d', strtotime("-1 day")),
-  );
+  if ($limit == NULL) {
+    $get_latest_translations .= " AND t.time > :from_date ORDER BY t.time DESC";
+    $args[':from_date'] = date('Y-m-d', strtotime("-$days days"));
+  }
+  else {
+    $get_latest_translations .= " ORDER BY t.time DESC LIMIT $limit";
+  }
 
   // run the query and get the translations
+  print($get_latest_translations."\n");
+  print_r($args);
   $translations = qtr::db_query($get_latest_translations, $args)->fetchAll();
 
   return $translations;
