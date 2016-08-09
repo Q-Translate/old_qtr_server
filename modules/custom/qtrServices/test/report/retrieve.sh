e6b4dcb6d27b5da87e67ecac1c502af495350e76
Dashamir Hoxha <dashohoxha@gmail.com>
2016-08-09 02:15:23 +0200

    Cleanup obsolete files.

--- modules/custom/qtrServices/test/report/retrieve.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### general contribution statistics
-curl -k -i -X GET -H "Accept: application/json"  \
-     "https://dev.qtranslate.org/api/report/statistics?lng=sq"
-
-### top contributors
-curl -k -i -X GET -H "Accept: application/json"  \
-     "https://dev.qtranslate.org/api/report/topcontrib?lng=sq&period=week&size=5"
--- modules/custom/qtrServices/test/report/statistics.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -k -i -H "Content-type: application/json"  \
-     -X POST https://dev.qtranslate.org/api/
-report/statistics.json  \
-     -d '{"lng": "sq"}'
